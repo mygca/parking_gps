c83dfca3f41de88a3b52681385922c73d2cd4bb7
Christina <christina.stephan@hetic.net>
2020-02-14 09:08:24 +0100

    unput state

--- react/components/Sidebar/Resultat/Filter/Input.jsx
+++ react/components/Sidebar/Resultat/Filter/Input.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 function Input({name,minHeight,setMinHeight}) {
 
-  function handlerOnChange(e){
-    setMinHeight(e);
+  function handlerOnChange(event){
+    setMinHeight(event.target.value);
   }
 
   return (
     <div className="wrapper--input">
-      <input placeholder="1.9" onChange={(e) => {handlerOnChange(e)}} />
+      <input placeholder="1.9" onChange={handlerOnChange} />
       <label>{name}</label>
+      <p>{minHeight}</p>
     </div>
   );
 }
