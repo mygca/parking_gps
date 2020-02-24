3a5770110b3d8f398b85ce9efeca8ea4b622b2ea
Christina <christina.stephan@hetic.net>
2020-02-24 17:56:51 +0100

    filter parking on map

---
+++ react/functions/map/filterParkingMap.js
@@ -0,0 +1,43 @@
+export default function filterParkingMap({showParkingsBox}){
+  
+                  console.log("what match!!!!!")
+                console.log(showParkingsBox.length);
+
+                let circles = document.querySelectorAll("circle");
+                for (let i = 0; i < circles.length; i++) {
+                  circles[i].style.display="none";
+                }
+
+                let matchedGares =[];
+                for (let i = 0; i < showParkingsBox.length; i++) {
+                  //console.log(i);
+                  let matchedGareId = showParkingsBox[i];
+                  if(matchedGares.indexOf(matchedGareId)===-1){
+                    matchedGares.push(matchedGareId.gares_id)
+                    console.log("new")
+                  }
+                }
+                console.log("filtered Array "+matchedGares)
+                let mattchedArray= matchedGares.flat().map(String)
+                //console.log(mattchedArray);
+
+                
+
+                  for (let i = 0; i < mattchedArray.length; i++) {
+
+                  
+                    const mgi = mattchedArray[i];
+                    if (mgi != null) {
+                      
+    
+                      let elementTest=document.querySelector('[data-station-id="'+mgi+'"]')
+                      //console.log(elementTest);
+                      //console.log(elementTest);
+                      if (elementTest != null) {
+                        elementTest.style.display="block";
+                        elementTest.style.fill="red";
+                      }
+                      
+                    }
+                  }
+}
